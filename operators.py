4a3a0dc31596f31d79fd757d9c2c03cf9ad43ac4
Janakiram6604 <ram.reddy6604@gmail.com>
2026-07-08 15:18:50 +0530

    day3

--- operators.py
+++ operators.py
@@ -177,4 +177,115 @@
 # print(student)
 
 ##################
-stude
+# students={'ram', 'shyam', 'hari', 'gita'}
+# print(students)
+# num=[1, 2, 3, 4, 5, 6]
+# print(num[-3])
+
+###################
+###data=(1, 2, 3, 4, 5)
+##data[0]=10
+#print(data)
+# x=(1, 2,2,2, 3)
+# print(x.count(2))
+
+################
+# X=('ram', 'shyam', 'hari', 'gita' , 'sita' , 'ram')
+# print(X.count('ram'))
+
+##############
+# num=(1, 2, 3, 4, 5)
+# print(num[1:4])  
+
+################
+#sets
+#sets
+# set is a collection of unique elements, meaning it does not allow duplicate values. Sets are unordered, which means that the items do not have a defined order, and they cannot be accessed by index. 
+# x={1, 2, 3, 4, 5}
+# print(x)
+
+# #############
+# a={1, 2, 3}
+# b={3, 4, 5}
+# print(a & b)  # intersection
+# print(a | b)  # union
+# print(a - b)  # difference
+# print(a ^ b)  # symmetric difference
+
+#####################
+# def add(*numbers):
+#     total =0
+#     for i in numbers:
+#         total += i
+#     print("Sum of numbers:", total)
+# add(10, 20, 30, 40)
+
+################
+# def squareroot(num):
+#     return num ** 0.5
+# print(squareroot(16))
+
+##################
+# def square(num):
+#      return num ** 2
+# print(square(6))
+
+####################
+# square=lambda x:x*x
+# print(square(12))
+
+#####################
+# check_num = lambda x: "Even" if x % 2 == 0 else "Odd"
+# print(check_num(7))   
+# print(check_num(12))
+
+######################
+# to_upper = lambda x: x.upper()
+# to_lower = lambda x: x.lower()
+
+# print(to_upper("hello"))  
+# print(to_lower("WORLD"))
+
+#######################
+# file = open("test.txt", "w")
+# file.write("hello")
+# file.close()
+# print("file created successfully")
+
+# file = open("test.txt", "r")
+# data = file.read()
+# print(data)
+# file.close()
+
+# file = open("test.txt", "a")
+# file.write("\nworld")
+# print("data appended successfully")
+# file.close()
+
+# file = open("test.txt", "r")
+# print(file.read())
+# file.close()
+
+#####################
+# try:
+#     num = int(input("Enter a number: "))
+#     print("You entered:", num)
+# except ValueError:
+#     print("only numbers are allowed")
+
+# try:   
+#     a=int(input("enter a number"))
+#     b=int(input("enter a number"))
+#     print(a/b)
+# except ZeroDivisionError:
+#     print("Error: Division by zero is not allowed.")
+# except ValueError:
+#     print("Error: Invalid input. Please enter a valid integer.")
+
+try:
+    file =open("non_existent_file.txt", "r")
+    print(file.read())
+except FileNotFoundError:
+    print("Error: The file does not exist.")
+finally:
+    print("File operation completed.")
