053489c70bd955f6bcb6ad51829ed26b74419f2d
Janakiram6604 <ram.reddy6604@gmail.com>
2026-07-17 08:57:13 +0530

    new

--- demo/demoapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('function/', views.hello_world, name='hello_world'),
-    path('class/', views.helloindia.as_view(), name='helloindia'),
-]
---
+++ django/demo/demoapp/views.py
@@ -0,0 +1,23 @@
+from pydoc import render_doc
+
+from django.http import HttpResponse
+from django.views import View
+
+from django.forms import ReservationForm
+
+
+def hello_world(request):
+    return HttpResponse("hello word")
+
+
+class helloindia(View):
+    def get(self, request):
+        return HttpResponse("hello india")
+    def home(self, request):
+        form = ReservationForm()
+        if request.method == 'POST' :
+            form = ReservationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return HttpResponse("success")
+        return render_doc(request,'index.html',{'form' : form})
